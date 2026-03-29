dbcc32038f4fa62288da7df811f4944032a104b5
Mihajlo Melar <m1h4jlomelar@gmail.com>
2026-03-30 01:32:57 +0200

    dodata stranica za prikaz ucesnika

---
+++ UcesniciStampa.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UcesniciStampa.aspx.cs" Inherits="seminarskiVP.UcesniciStampa" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Štampa - Lista učesnika</title>
+    <meta charset="utf-8" />
+    <style>
+        body { font-family: Arial; margin: 40px; }
+        h2 { text-align: center; }
+        table { width: 100%; border-collapse: collapse; margin-top: 20px; }
+        th, td { border: 1px solid #333; padding: 8px; text-align: left; }
+        th { background-color: #f2f2f2; }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Štampa - Lista svih učesnika</h2>
+        <asp:GridView ID="gvUcesnici" runat="server" AutoGenerateColumns="False" GridLines="Both" BorderStyle="Solid">
+            <Columns>
+                <asp:BoundField DataField="ID" HeaderText="ID" />
+                <asp:BoundField DataField="Ime" HeaderText="Ime" />
+                <asp:BoundField DataField="Prezime" HeaderText="Prezime" />
+                <asp:BoundField DataField="Email" HeaderText="Email" />
+            </Columns>
+        </asp:GridView>
+    </form>
+
+
+    <div class="center">
+         <a href="Default.aspx" class="btn">🏠 Vrati se na početnu</a>
+    </div>
+
+
+
+</body>
+</html>
---
+++ UcesniciStampa.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace seminarskiVP
+{
+    public partial class UcesniciStampa : System.Web.UI.Page
+    {
+        protected void Page_Load( object sender, EventArgs e )
+        {
+            if ( !IsPostBack )
+            {
+                UcitajUcesnike();
+            }
+        }
+
+        private void UcitajUcesnike()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["KonekcijaString"].ConnectionString;
+
+            using ( SqlConnection conn = new SqlConnection(cs) )
+            {
+                string query = @"SELECT DISTINCT U.ID, U.Ime, U.Prezime, U.Email
+                                FROM Ucesnik U
+                                INNER JOIN Zaduzenje Z ON U.ID = Z.IDUcesnika
+                                INNER JOIN Projekat P ON Z.IDProjekta = P.ID
+                                ORDER BY U.Prezime, U.Ime;
+                                ";
+
+                using ( SqlCommand cmd = new SqlCommand(query, conn) )
+                {
+                    conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    gvUcesnici.DataSource = dr;
+                    gvUcesnici.DataBind();
+                }
+            }
+        }
+    }
+}
